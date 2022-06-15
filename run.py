3bf5abbc5e72acba0d598ede81554349df75ba06
Tasha Johnson <tashataylorjohnson@me.com>
2022-06-15 17:34:00 +0000

    Import pyfiglet, colorama and pyinputplu

--- run.py
+++ run.py
@@ -2,7 +2,16 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread 
+import pyfiglet
+import pyfiglet.fonts
+import colorama
+import pkg_resources
+import os
+from colorama import Fore, Back, Style
 from google.oauth2.service_account import Credentials 
+from pyfiglet import figlet_format
+import pyinputplus as pyip
+colorama.init(autoreset=True)
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -15,5 +24,4 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('birthday_book_v2')
 BIRTHDAY_WORKSHEET = SHEET.worksheet('birthdays')
-
 
